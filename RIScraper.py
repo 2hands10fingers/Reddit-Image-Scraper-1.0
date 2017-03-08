7d7eca491838c3a508d2b69e75551a4ae006839e
Tono <antonio.nogueras@yahoo.com>
2017-03-08 16:06:19 -0600

    Create RIScraper.py

---
+++ RIScraper.py
@@ -0,0 +1,11 @@
+import praw
+import config
+
+def bot_login():
+    r = praw.Reddit(username =config.username,
+                password=config.password
+                client_secret=config.client_secret,
+                client_id=config.client_id,
+                user_agent="RedditImageScraper by /u/2hands10fingers, /u/Joe_Anonimist, /u/iakovosbelonias")
+
+r = bot_login()
