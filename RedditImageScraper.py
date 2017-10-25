17206f54f30a1b46fb9d37eedf63a216c36f9b07
Trevor Christiansen <trevor@tjc.im>
2017-10-25 10:20:10 -0600

    Moved everything into main

--- RedditImageScraper.py
+++ RedditImageScraper.py
@@ -21,12 +21,12 @@ def time_clear_header1():
     clear_screen()
     print("\t  .___________________________________________. \n \t / \t \t \t \t \t" + "       \\" + "" + "\n\t|    Welcome to the Reddit Image Scraper 1.0\t| \n \t| \t \tby pyStudyGroup \t \t" + "" + "|" + "\n\t +~____________________________________________~+ \n")
 
-def time_clear_headerSTART():
+def time_clear_headerSTART(month_b, day_b, year_b):
     time.sleep(0.9)
     clear_screen()
     print("\t  .___________________________________________. \n \t / \t \t \t \t \t" + "       \\" + "" + "\n \t|    Welcome to the Reddit Image Scraper 1.0\t| \n \t| \t   DATE RANGE: "+ str(month_b) + "-" + str(day_b) + "-" + str(year_b)+ " -- " "\t \t" + "" + "|" + "\n\t +~____________________________________________~+ \n")
 
-def time_clear_headerEND():
+def time_clear_headerEND(month_b, day_b, year_b, month_e, day_e, year_e):
     time.sleep(0.9)
     clear_screen()
     print("\t  .___________________________________________. \n \t / \t \t \t \t \t" + "       \\" + "" + "\n \t|    Welcome to the Reddit Image Scraper 1.0\t| \n \t| \t DATE RANGE: "+ str(month_b) + "-" + str(day_b) + "-" + str(year_b)+ " -- " + str(month_e) + "-" + str(day_e) + "-" + str(year_e) + "\t" + "" + "|" + "\n\t +~____________________________________________~+ \n")
@@ -63,95 +63,6 @@ def set_date_range(month):
 def valid(selected_date, valid_range):
     return (selected_date in valid_range)
 
-########################
-###     CONSTANTS    ###
-########################
-
-#Values
-year_range = range(2005, int(datetime.now().year)+1)
-month_range = range(1, 13)
-
-#Messages
-year_start_msg = '\n\tEnter the year you would like to START your range: \n\t'
-year_end_msg = '\n\tEnter the year you would like to END your range: \n\t'
-
-month_msg = '\n\tNow, how about a month: '
-
-day_msg = '\n\tLastly, enter the day: '
-
-########################
-### START DATE LOGIC ###
-########################
-
-time.sleep(0.2)
-time_clear_header1()
-
-year_b = validate_date(year_range, year_start_msg)
-
-print ("\n\tExcellent! " + str(year_b) + " is a great year.")
-
-time_clear_header1()
-
-month_b = validate_date(month_range, month_msg)
-
-print ("\n\tWe'll accept that")
-time_clear_header1()
-
-date_range = set_date_range(month_b)
-
-day_b = validate_date(date_range, day_msg)
-
-print ("\n\tLooks good to us.")
-print("\n\n\tYou have selected a start date of: " + str(month_b) + "-" + str(day_b) + "-" + str(year_b)+ "\n\n\tIs this correct? We hope so!")
-time.sleep(2.0)
-
-########################
-##### END DATE LOGIC ###
-########################
-
-time_clear_headerSTART()
-
-year_e = validate_date(year_range, year_end_msg)
-
-print ("\n\tExcellent! " + str(year_e) + " is a great year. Just like " + str(year_b) +"!")
-time_clear_headerSTART()
-
-month_e = validate_date(month_range, month_msg)
-
-print ("\n\tWe'll accept that")
-time_clear_header1()
-time_clear_headerSTART()
-
-date_range = set_date_range(month_e)
-
-day_e = validate_date(date_range, day_msg)
-
-print ("\n\tLooks good to us.")
-time_clear_headerEND()
-
-designatedSubReddit = input('\n\t At last, what subreddit would you like to scrape? \n\n\t www.reddit.com\\r\\')
-
-print("\n\t \t   Downloading images from /r/" + str(designatedSubReddit) + "... \n\n\t    ___________________________________________\n\n \t\t\t\t*** \n")
-time.sleep(2.0)
-
-########################
-##### INPUT PROCESSING #
-########################
-
-# Y / M / D / H / Mi / S / ??
-# (2017, 3, 9, 23, 49, 20, 000000), timezone='GMT')
-
-bd_dt = Delorean(datetime=datetime(year_b, month_b, day_b, 0, 0, 0, 000000),  timezone='GMT')
-bd_epoch = bd_dt.epoch
-print(bd_epoch)
-
-ed_dt = Delorean(datetime=datetime(year_e, month_e, day_e, 23, 59, 0, 000000),  timezone='GMT')
-ed_epoch = ed_dt.epoch
-print(ed_epoch)
-
-# Messages for the downlad_file() function
-MSG_START = 'Downloading file: {}.'
-MSG_END = '\t{} downloaded in {} seconds.\n'
 
 # Returns a list of urls posted to the subreddit_name
 # between start_date and end_date.
@@ -193,7 +104,7 @@ def urls_by_period(subreddit_name, start_date, end_date):
 # If verbose is True the script prints:
 # The name of the file being downloaded
 # The file download time.
-def download_file(url, date_created, verbose=False):
+def download_file(MSG_START, MSG_END, url, date_created, verbose=False):
 
     filename = date_created + str(url).split('/')[-1]
 
@@ -215,6 +126,96 @@ def download_file(url, date_created, verbose=False):
         dl_time = datetime.now()
 
 def main():
+    ########################
+    ###     CONSTANTS    ###
+    ########################
+
+    # Messages for the downlad_file() function
+    MSG_START = 'Downloading file: {}.'
+    MSG_END = '\t{} downloaded in {} seconds.\n'
+
+    #Values
+    year_range = range(2005, int(datetime.now().year)+1)
+    month_range = range(1, 13)
+
+    #Messages
+    year_start_msg = '\n\tEnter the year you would like to START your range: \n\t'
+    year_end_msg = '\n\tEnter the year you would like to END your range: \n\t'
+
+    month_msg = '\n\tNow, how about a month: '
+
+    day_msg = '\n\tLastly, enter the day: '
+
+    ########################
+    ### START DATE LOGIC ###
+    ########################
+
+    time.sleep(0.2)
+    time_clear_header1()
+
+    year_b = validate_date(year_range, year_start_msg)
+
+    print ("\n\tExcellent! " + str(year_b) + " is a great year.")
+
+    time_clear_header1()
+
+    month_b = validate_date(month_range, month_msg)
+
+    print ("\n\tWe'll accept that")
+    time_clear_header1()
+
+    date_range = set_date_range(month_b)
+
+    day_b = validate_date(date_range, day_msg)
+
+    print ("\n\tLooks good to us.")
+    print("\n\n\tYou have selected a start date of: " + str(month_b) + "-" + str(day_b) + "-" + str(year_b)+ "\n\n\tIs this correct? We hope so!")
+    time.sleep(2.0)
+
+    ########################
+    ##### END DATE LOGIC ###
+    ########################
+
+    time_clear_headerSTART(month_b=month_b, day_b=day_b, year_b=year_b)
+
+    year_e = validate_date(year_range, year_end_msg)
+
+    print ("\n\tExcellent! " + str(year_e) + " is a great year. Just like " + str(year_b) +"!")
+    time_clear_headerSTART(month_b=month_b, day_b=day_b, year_b=year_b)
+
+    month_e = validate_date(month_range, month_msg)
+
+    print ("\n\tWe'll accept that")
+    time_clear_header1()
+    time_clear_headerSTART(month_b=month_b, day_b=day_b, year_b=year_b)
+
+    date_range = set_date_range(month_e)
+
+    day_e = validate_date(date_range, day_msg)
+
+    print ("\n\tLooks good to us.")
+    time_clear_headerEND(month_b=month_b, day_b=day_b, year_b=year_b, month_e=month_e, day_e=day_e, year_e=year_e)
+
+    designatedSubReddit = input('\n\t At last, what subreddit would you like to scrape? \n\n\t www.reddit.com\\r\\')
+
+    print("\n\t \t   Downloading images from /r/" + str(designatedSubReddit) + "... \n\n\t    ___________________________________________\n\n \t\t\t\t*** \n")
+    time.sleep(2.0)
+
+    ########################
+    ##### INPUT PROCESSING #
+    ########################
+
+    # Y / M / D / H / Mi / S / ??
+    # (2017, 3, 9, 23, 49, 20, 000000), timezone='GMT')
+
+    bd_dt = Delorean(datetime=datetime(year_b, month_b, day_b, 0, 0, 0, 000000),  timezone='GMT')
+    bd_epoch = bd_dt.epoch
+    print(bd_epoch)
+
+    ed_dt = Delorean(datetime=datetime(year_e, month_e, day_e, 23, 59, 0, 000000),  timezone='GMT')
+    ed_epoch = ed_dt.epoch
+    print(ed_epoch)
+
 
     urls = urls_by_period(
             designatedSubReddit,
@@ -229,8 +230,8 @@ def main():
         url = url_list[0]
         date_created = url_list[1]
         if url.endswith(('.jpg', '.png')):
-            download_file(url, date_created, verbose=True)
-     
+            download_file(MSG_START, MSG_END, url, date_created, verbose=True)
+
     print("\n Thanks for using Reddit Image Scraper 1.0 \n")
 
 
