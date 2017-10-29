0909ceb2cf051e7490be3127565663b7d7fefce4
Tono <antonio.nogueras@yahoo.com>
2017-10-29 00:38:21 -0500

    Cleaned up!

--- redditaccess.py
+++ redditaccess.py
@@ -2,14 +2,16 @@ import praw
 import config
 
 def bot_login():
-    r = praw.Reddit(username =config.username,
-                password=config.password,
-                client_secret=config.client_secret,
-                client_id=config.client_id,
-                user_agent="RedditImageScraper by /u/2hands10fingers, /u/Joe_Anonimist, /u/iakovosbelonias")
-                
+
+    r = praw.Reddit(username = config.username,
+                    password = config.password,
+                    client_secret = config.client_secret,
+                    client_id = config.client_id,
+                    user_agent = "RedditImageScraper by /u/2hands10fingers, /u/Joe_Anonimist, /u/iakovosbelonias")
     return r
 
 if __name__ == '__main__':
+
     r = bot_login()
+
     print(r)
